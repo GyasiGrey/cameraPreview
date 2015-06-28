e4c77eb5e484413726370c769d9c7624e309789a
gyasi <34169798+GyasiGrey@users.noreply.github.com>
2015-06-28 00:30:17 +0000

    Changed dng to ARW because ..... ARW?

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 					foreach($imgs as $i) {
 						$index++;
 
-						$dataTitle = "<a href='/cameraDump/" . $dir . "/" . str_replace('jpg', 'dng', $i) . "' target='_blank'>Get raw file</a>";
+						$dataTitle = "<a href='/cameraDump/" . $dir . "/" . str_replace('jpg', 'ARW', $i) . "' target='_blank'>Get raw file</a>";
 						echo '<a class="example-image-link" href="/cameraDump/' . $dir . '/JPEG/' . $i . '" data-lightbox="previewGroup" data-title="' . $dataTitle . '"><img class="example-image" src="/cameraDump/' . $dir . '/JPEG/' . $i . '" alt="/' . $i . '" /></a>';
 					}
 				?>
